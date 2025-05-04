812e0ba3e2c08bddd4fead220dc75ddb1ed2748b
VirtualRohit29 <rohitkr290704@gmail.com>
2025-05-04 15:27:40 +0530

    Updated something

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='text-red-600'>App</div>
+    <>
+    <Navbar></Navbar>
+    </>
   )
 }
 
